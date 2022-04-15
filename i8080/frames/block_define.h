23ca4366d6d1552626a7fd637054db4c524549ab
Sergei Dolin <dlinyj@gmail.com>
2022-04-15 22:23:22 +0300

    Add block define

---
+++ i8080/frames/block_define.h
@@ -0,0 +1,59 @@
+// X-
+// --
+#define TOP_LEFT_POINT		0x01
+
+// -X
+// --
+#define TOP_RIGHT_POINT		0x02
+
+// XX
+// --
+#define UP_HALF_OF_BLOK		0x03
+
+// --
+// -X
+#define BOT_RIGHT_POINT		0x04
+
+// X-
+// -X
+#define DIAGONAL_FLTR		0x05
+
+// -X
+// -X
+#define VERT_RIGHT			0x06
+
+// XX
+// -X
+#define BOT_LEFT_POINT_B	0x07
+
+// --
+// X-
+#define BOT_LEFT_POINT		0x10
+
+// X-
+// X-
+#define VERT_LEFT 			0x11
+
+// -X
+// X-
+#define DIAGONAL_FRTL		0x12
+
+// XX
+// X-
+#define BOT_RIGHT_POINT_B	0x13
+
+// --
+// XX
+#define DOWN_HALF_OF_BLOK	0x14
+
+// X-
+// XX
+#define TOP_RIGHT_POINT_B	0x15
+
+// -X
+// XX
+#define TOP_LEFT_POINT_B	0x16
+
+// XX
+// XX
+#define FULL_BLOCK			0x17
